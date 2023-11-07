6e94c19795527e67811930d69551d440dea34c5c
YukiNeko27 <firsthalf200@gmail.com>
2023-11-07 15:12:01 +0700

    8th commit

---
+++ algoflowgram/script.js
@@ -0,0 +1,12 @@
+alert("Pintu rumah terkunci.");
+
+let confirm1 = confirm("Cek kantong celana?");
+
+if(confirm1 == true){
+    alert("Kunci Ditemukan.")
+}else{
+    confirm("Tendang pintu sampai terbuka?");{
+        alert("Pintu terbuka, tetapi mekanisme kuncinya rusak.")
+    }
+}
+
