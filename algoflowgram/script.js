114b9c3bbaf9ba426eb44129f20b565a30e32586
unknown <firsthalf200@gmail.com>
2023-11-07 18:34:47 +0700

    11th commit

--- algoflowgram/script.js
+++ algoflowgram/script.js
@@ -3,12 +3,15 @@ alert("Komputer tidak menyala.")
 let confirm1 = confirm("Apakah kabel dicolok ke stop kontak?")
 
 if(confirm1 == true){
-    alert("Pasang kabel power.")
+    alert("Pasang kabel power.");
+    alert("Algorithma berakhir.");
 }else{
-    let confirm2 = alert("Apakah kipas (cooler) computer berputar?")
+    let confirm2 = confirm("Apakah kipas (cooler) computer berputar?");
     if(confirm2 == true){
-        alert("Kemungkinan monitor yang bermasalah, perbaiki monitor.")
+        alert("Kemungkinan monitor yang bermasalah, perbaiki monitor.");
+        alert("Algoritma berakhir.");
     }else{
-        alert("Komputer bermasalah, perbaiki komputer!")
+        alert("Komputer yang bermasalah, perbaiki komputer!");
+        alert("Algoritma berakhir.");
     }
 }
