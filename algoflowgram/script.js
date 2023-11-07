ff3824cd39ca502d083a0f9ade0ab9741cc4a2f0
unknown <firsthalf200@gmail.com>
2023-11-07 18:21:50 +0700

    9th commit

--- algoflowgram/script.js
+++ algoflowgram/script.js
@@ -1,12 +1,14 @@
-alert("Pintu rumah terkunci.");
+alert("Komputer tidak menyala.")
 
-let confirm1 = confirm("Cek kantong celana?");
+let confirm1 = confirm("Apakah kabel dicolok ke stop kontak?")
 
 if(confirm1 == true){
-    alert("Kunci Ditemukan.")
+    alert("Pasang kabel power.")
 }else{
-    confirm("Tendang pintu sampai terbuka?");{
-        alert("Pintu terbuka, tetapi mekanisme kuncinya rusak.")
+    let confirm2 = alert("Apakah kipas (cooler) computer berputar?")
+    if(confirm2 == true){
+        alert("Kemungkinan monitor yang bermasalah, perbaiki monitor.")
+    }else{
+        alert("Komputer bermasalah, perbaiki komputer!")
     }
 }
-
